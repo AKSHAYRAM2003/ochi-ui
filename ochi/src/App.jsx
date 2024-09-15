f8092e1824352ef7c0c60acfe9cd1d5c21757201
Akshay  H R <ramakshay100@gmail.com>
2024-09-15 18:18:19 +0530

    final modification done added all files

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import LandingPage from './Components/LandingPage'
+import Marquee from './Components/Marquee'
+import About from './Components/About'
+import Eyes from './Components/Eyes'
+import Featured from './Components/Featured'
+import Cards from './Components/Cards'
+import Footer from './Components/Footer'
+
 function App() {
   return (
     <div className='w-full h-screen '>
     <Navbar/> 
     <LandingPage/>
+    <Marquee/>
+    <About/>
+    <Eyes/>
+    <Featured/>
+    <Cards/>
+    <Footer/>
     </div>
   )
 }
